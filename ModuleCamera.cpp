00a425f84e78e08b41bf7d1b82b57027210cadb3
Sergi Ledesma Sánchez <sergils2@ttc.cat>
2017-02-02 20:08:02 +0100

    SetFOV, SetAspectRatio, SetPlaneDistances

--- ModuleCamera.cpp
+++ ModuleCamera.cpp
@@ -1,14 +1,29 @@
 #include "ModuleCamera.h"
+#include "MathGeoLib/include/MathGeoLib.h"
+#include <math.h>
 
 
 
-ModuleCamera::ModuleCamera()
+ModuleCamera::ModuleCamera(const JSON_Object *json) : Module(json)
 {
+	frustum = new Frustum();
+	verticalFov = 2 * atan(tan(horizontalFov * 0.5) * 1 / aspectRatio);
+	horizontalFov = 2 * atan(tan(verticalFov * 0.5) * aspectRatio);
 }
 
 
 ModuleCamera::~ModuleCamera()
 {
+}
+
+bool ModuleCamera::Init()
+{
+	return true;
+}
+
+bool ModuleCamera::Start()
+{
+	return true;
 }
 
 update_status ModuleCamera::PreUpdate(float dt)
@@ -26,17 +41,27 @@ update_status ModuleCamera::PostUpdate(float dt)
 	return UPDATE_CONTINUE;
 }
 
-bool ModuleCamera::Init()
-{
-	return true;
-}
-
-bool ModuleCamera::Start()
-{
-	return true;
-}
-
 bool ModuleCamera::CleanUp()
 {
 	return true;
+}
+
+void ModuleCamera::SetFOV(float verticalFov)
+{
+	this->verticalFov = verticalFov;
+	frustum->SetVerticalFovAndAspectRatio(verticalFov, this->aspectRatio);
+}
+
+void ModuleCamera::SetAspectRatio(float aspectRatio)
+{
+	this->aspectRatio = aspectRatio;
+	horizontalFov = 2 * atan(tan(verticalFov * 0.5) * aspectRatio);
+	frustum->SetHorizontalFovAndAspectRatio(this->horizontalFov, aspectRatio);
+}
+
+void ModuleCamera::SetPlaneDistances(float near, float far)
+{
+	this->near = near;
+	this->far = far;
+	frustum->SetViewPlaneDistances(near, far);
 }
--- ModuleCamera.h
+++ ModuleCamera.h
@@ -3,11 +3,13 @@
 
 
 #include "Module.h"
+
+
 class ModuleCamera :
 	public Module
 {
 public:
-	ModuleCamera();
+	ModuleCamera(const JSON_Object *json);
 	~ModuleCamera();
 	bool Init();
 	bool Start();
@@ -15,6 +17,17 @@ public:
 	update_status Update(float dt);
 	update_status PostUpdate(float dt);
 	bool CleanUp();
+	void SetFOV(float verticalFov);
+	void SetAspectRatio(float aspectRatio);
+	void SetPlaneDistances(float near, float far);
+
+private:
+	Frustum *frustum = nullptr;
+	float aspectRatio = 1.78;
+	float verticalFov = 30;
+	float horizontalFov = 30;
+	float near = NULL;
+	float far = NULL;
 
 };
 
