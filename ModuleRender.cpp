b81b28936f8e9aeaf5dc33ed1e42ea4c984d1bab
Sergi Ledesma Sánchez <sergils2@ttc.cat>
2017-02-09 19:06:47 +0100

    Basic gizmo at 0,0,0

--- ModuleRender.cpp
+++ ModuleRender.cpp
@@ -162,6 +162,46 @@ update_status ModuleRender::Update(float dt)
 	glDisableClientState(GL_VERTEX_ARRAY);
 	glDisableClientState(GL_COLOR_ARRAY);
 
+	//Draw Gyzmo
+	glBegin(GL_LINES);
+	glLineWidth(5.0f);
+	glColor4f(5.0f, 0.0f, 0.0f, 1.0f); // Red
+	//Line
+	glVertex3f(0, 0, 0);
+	glVertex3f(1, 0, 0);
+	//Letter
+	glVertex3f(1.2f, 0.1f, 0.1f);
+	glVertex3f(1.2f, -0.1f, -0.1f);
+	glVertex3f(1.2f, 0.1f, -0.1f);
+	glVertex3f(1.2f, -0.1f, 0.1f);
+
+	glColor4f(0.0f, 5.0f, 0.0f, 1.0f); // Green
+	//Line
+	glVertex3f(0, 0, 0);
+	glVertex3f(0, 1, 0);
+	//Letter
+	glVertex3f(0.2f, 1.2f, 0);
+	glVertex3f(0, 1.2f, 0);
+	glVertex3f(0, 1.2f, 0);
+	glVertex3f(-0.1f, 1.2f, -0.1f);
+	glVertex3f(0, 1.2f, 0);
+	glVertex3f(-0.1f, 1.2f, 0.1f);
+
+	glColor4f(0.0f, 0.0f, 5.0f, 1.0f); // Blue
+	//Line
+	glVertex3f(0, 0, 0);
+	glVertex3f(0, 0, 1);
+	//Letter
+	glVertex3f(0.1f, 0.1f, 1.2f);
+	glVertex3f(-0.1f, 0.1f, 1.2f);
+	glVertex3f(-0.1f, 0.1f, 1.2f);
+	glVertex3f(0.1f, -0.1f, 1.2f);
+	glVertex3f(0.1f, -0.1f, 1.2f);
+	glVertex3f(-0.1f, -0.1f, 1.2f);
+
+	glEnd();
+	glLineWidth(1.0f);
+
 	//Draw Grid
 	glBegin(GL_LINES);
 	glLineWidth(2.0f);
